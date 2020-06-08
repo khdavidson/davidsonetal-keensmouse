724b2085682cad08a167778f0a6f804a5bd9a228
kdavidson <katie.davidson92@gmail.com>
2020-06-07 19:35:01 -0700

    added combo plot code and reformatted from old code

--- distshore_hair.R
+++ distshore_hair.R
@@ -6,43 +6,239 @@ setwd("~/UVic/`Field Work 2016/`RESULTS/Data files")
 hair.data = read.csv("fieldmouse_distshore_hair.csv")
 
 library(tidyverse)
+library(FSA)    # for Dunn test
 
 #######################################################################################################################################################
 
-# create new column for indexing by node 
-hair.data$Node = "CV"
-hair.data$Node[hair.data$Region=="GS"]<-"GS"
+#                                                           DATA CLEANING
+
+hair.data <- hair.data %>% 
+  rename(individual_id=Sample.ID, 
+         region=Region) %>% 
+  mutate(dist_group = ifelse(loc_frst_cap =="0"|loc_frst_cap=="25", "0-25", 
+                             ifelse(loc_frst_cap=="50"|loc_frst_cap=="75", "50-75",
+                                    ifelse(loc_frst_cap=="100"|loc_frst_cap=="125", "100-125", "150-200")))) %>%  
+  print()
+
+
+#######################################################################################################################################################
+
+#                                                           STATISTICAL COMPARISONS
+
+# ----------------------------- NITROGEN 
+
+#####
+# TEST ASSUMPTIONS
+#####
+lmN <- lm(hair.data$d15N ~ hair.data$dist_group)
+rN <- resid(lmN)
+plot(rN)
+hist(rN)
+qqnorm(rN)
+qqline(rN)
+
+# log-transform does it improve? -- NO
+lmN.l <- lm(log(hair.data$d15N) ~ hair.data$dist_group)
+rN.l <- resid(lmN.l)
+plot(rN.l)
+hist(rN.l)
+qqnorm(rN.l)
+qqline(rN.l)
+
+# compare results of anova and kruskal-wallis -- Both cases very non-significant
+summary(aovN <- aov(hair.data$d15N ~ hair.data$dist_group))
+TukeyHSD(aovN)
+
+kwN <- kruskal.test(hair.data$d15N ~ hair.data$dist_group)
+dunnTest(hair.data$d15N ~ hair.data$dist_group)
+
+
+# ----------------------------- CARBON 
+
+#####
+# TEST ASSUMPTIONS
+#####
+lmC <- lm(hair.data$d13C ~ hair.data$dist_group)
+rC <- resid(lmC)
+plot(rC)
+hist(rC)
+qqnorm(rC)
+qqline(rC)
+
+# log-transform does it improve? -- YES
+lmC.l <- lm(log(hair.data$d13C*-1) ~ hair.data$dist_group)
+rC.l <- resid(lmC.l)
+plot(rC.l)
+hist(rC.l)
+qqnorm(rC.l)
+qqline(rC.l)
+
+# compare results of anova and kruskal-wallis -- Both cases very non-significant
+summary(aovC <- aov(hair.data$d13C ~ hair.data$dist_group))
+TukeyHSD(aovC)
+
+kwC <- kruskal.test(hair.data$d13C ~ hair.data$dist_group)
+kwC
+dunnTest(hair.data$d13C ~ hair.data$dist_group)
 
 
 
-## d13C 
+
+#######################################################################################################################################################
+
+#                                                           PLOTS
+
+hair.summary <- hair.data %>% 
+  group_by(dist_group) %>%
+  summarize(meanC = mean(d13C), seC = sd(d13C)/sqrt(length(d13C)), 
+            meanN = mean(d15N), seN = sd(d15N)/sqrt(length(d15N))) %>%
+  print()
+hair.summary$dist_group <- factor(hair.summary$dist_group, levels=c("0-25", "50-75", "100-125", "150-200"), ordered=T)
+
+
+# ----------------------------- NITROGEN 
+
 scaleFUN <- function(x) sprintf("%.1f", x)
 
-ggplot(data = hair.data,
-              aes(x = loc_frst_cap,
-                  y = d13C, colour=Region)) + 
-  ylim(-30,-17) +
-  labs(x = "Distance from the beach (m)",
-       y = "") +
-  ggtitle("") + 
-  geom_point(data=hair.data, aes(colour = Region), size=12) +
-  scale_x_continuous(breaks = c(0,25,50,75,100,125,150,175,200)) +
+ggplot(data=hair.summary, aes(x=dist_group, y=meanN)) + 
+  #ylim(-30,-17) +
+  geom_errorbar(aes(ymin=meanN-seN, ymax=meanN+seN), width=0, size=1.5, alpha=0.7) + 
+  geom_point(shape=21, size=7, stroke=1.5, fill="black") +
+  #scale_x_continuous(breaks = c(0,25,50,75,100,125,150,175,200)) +
   scale_y_continuous(labels=scaleFUN) + 
-  scale_color_manual(breaks = c("CV", "GS"),
-                     values=c("#ffaa00", "#4596ff")) +
-  theme(text = element_text(size=80)) + 
-  theme(panel.background = element_rect(fill = "black")) +
-  theme(panel.grid.minor = element_line(colour = "black",size=3)) + 
-  theme(panel.grid.major = element_line(colour = "white", linetype = "dotted", size = 1.2)) + 
-  theme(plot.background = element_rect(fill = "black")) + 
-  theme(axis.text = element_text(colour = "white")) + 
-  theme(axis.title = element_text(colour = "white")) + 
-  theme(panel.background = element_rect(colour = "white",size=3,linetype="solid")) + 
-  theme(panel.border = element_rect(colour = "white", fill=NA, size=3))+
-  theme(axis.ticks = element_line(size = 3, colour = "white")) +
-  theme(legend.position="none") +
-  geom_smooth(method='lm',formula=y~x,se=F,size=3) +
-  theme(plot.margin=unit(c(0,4,1,0),"cm"))
+  labs(x = "Distance from the beach (m)", y = "") +
+  ylab(expression(paste(delta^{15},'N (\211)'))) +
+  theme_bw() +
+  theme(axis.title = element_text(size=30, face = "bold"),
+        axis.title.y = element_text(margin=margin(t=2,l=0,r=4,b=0)),
+        axis.text = element_text(size=27, colour="black"),
+        axis.ticks = element_line(size=1),
+        axis.ticks.length = unit(1.5, "mm"),
+        panel.grid.major = element_blank(),#line(colour="gray80", size=0.8),
+        panel.grid.minor = element_blank(),
+        panel.border = element_rect(size=1.1),
+        legend.text = element_text(size=27), 
+        legend.title = element_blank(),
+        legend.position = c(0.88,0.88),
+        legend.background = element_rect(colour="black", size=0.8),
+        legend.key.size = unit(10, "mm"),
+        legend.margin = margin(t=-2,b=4,l=5,r=8))
+
+
+# ----------------------------- CARBON 
+
+scaleFUN <- function(x) sprintf("%.1f", x)
+
+ggplot(data=hair.summary, aes(x=dist_group, y=meanC)) + 
+  #ylim(-30,-17) +
+  geom_errorbar(aes(ymin=meanC-seC, ymax=meanC+seC), width=0, size=1.5, alpha=0.7) + 
+  geom_point(shape=21, size=7, stroke=1.5, fill="black") +
+  #scale_x_continuous(breaks = c(0,25,50,75,100,125,150,175,200)) +
+  scale_y_continuous(labels=scaleFUN) + 
+  labs(x = "Distance from the beach (m)", y = "") +
+  ylab(expression(paste(delta^{13},'C (\211)'))) +
+  theme_bw() +
+  theme(axis.title = element_text(size=30, face = "bold"),
+        axis.title.y = element_text(margin=margin(t=2,l=0,r=4,b=0)),
+        axis.text = element_text(size=27, colour="black"),
+        axis.ticks = element_line(size=1),
+        axis.ticks.length = unit(1.5, "mm"),
+        panel.grid.major = element_blank(),#line(colour="gray80", size=0.8),
+        panel.grid.minor = element_blank(),
+        panel.border = element_rect(size=1.1),
+        legend.text = element_text(size=27), 
+        legend.title = element_blank(),
+        legend.position = c(0.88,0.88),
+        legend.background = element_rect(colour="black", size=0.8),
+        legend.key.size = unit(10, "mm"),
+        legend.margin = margin(t=-2,b=4,l=5,r=8))
+
+#=========================================================================================== COMBO PLOTS
+
+# Combo plots of HAIR + POO
+# must have distshore_hair script open and have ran all of the intermediate tables prior to re-making these plots
+# data to use for hair: hair.summary     for poo: poo.summary
+# this is also duplicatedin the distshore_poo script
+
+poo.summary <- poo.summary %>%
+  select(dist_group:seN) %>%
+  mutate(source="Faeces") %>%
+  print()
+
+hair.summary <- hair.summary %>%
+  mutate(source="Hair") %>%
+  print()
+
+combo.summary <- rbind(poo.summary, hair.summary)
+combo.summary <- combo.summary %>%
+  arrange(desc(source)) %>%
+  print
+combo.summary$source <- factor(combo.summary$source, levels=c("Faeces", "Hair"), ordered=T)
+
+
+# ----------------------------- NITROGEN 
+
+scaleFUN <- function(x) sprintf("%.1f", x)
+
+ggplot(data=combo.summary, aes(x=dist_group, y=meanN)) + 
+  geom_errorbar(aes(ymin=meanN-seN, ymax=meanN+seN, colour=source), width=0, size=1.5) + 
+  geom_point(aes(fill=source), colour="black", size=7, stroke=1.3, shape=21) +
+  scale_fill_manual(values=c("gray80", "gray20")) +
+  scale_colour_manual(values=c("gray80", "gray20")) +
+  scale_y_continuous(labels=scaleFUN) +
+  labs(x = "Distance from beach (m)") +
+  ylab(expression(paste(delta^{15},'N (\211)'))) +
+  theme_bw() +
+  theme(axis.title = element_text(size=30, face = "bold"),
+        axis.title.y = element_text(margin=margin(t=2,l=0,r=4,b=0)),
+        axis.text = element_text(size=27, colour="black"),
+        axis.ticks = element_line(size=1),
+        axis.ticks.length = unit(1.5, "mm"),
+        panel.grid.major = element_blank(),#line(colour="gray80", size=0.8),
+        panel.grid.minor = element_blank(),
+        panel.border = element_rect(size=1.1),
+        legend.position = "none") 
+
+
+# ----------------------------- CARBON 
+
+scaleFUN <- function(x) sprintf("%.1f", x)
+
+ggplot(data=combo.summary, aes(x=dist_group, y=meanC)) + 
+  geom_errorbar(aes(ymin=meanC-seC, ymax=meanC+seC, colour=source), width=0, size=1.5) + 
+  geom_point(aes(fill=source), colour="black", size=7, stroke=1.3, shape=21) +
+  scale_fill_manual(values=c("gray80", "gray20")) +
+  scale_colour_manual(values=c("gray80", "gray20")) +
+  scale_y_continuous(labels=scaleFUN) +
+  labs(x = "Distance from beach (m)") +
+  ylab(expression(paste(delta^{13},'C (\211)'))) +
+  theme_bw() +
+  theme(axis.title = element_text(size=30, face = "bold"),
+        axis.title.y = element_text(margin=margin(t=2,l=0,r=4,b=0)),
+        axis.text = element_text(size=27, colour="black"),
+        axis.ticks = element_line(size=1),
+        axis.ticks.length = unit(1.5, "mm"),
+        panel.grid.major = element_blank(),#line(colour="gray80", size=0.8),
+        panel.grid.minor = element_blank(),
+        panel.border = element_rect(size=1.1),
+        legend.text = element_text(size=27), 
+        legend.title = element_blank(),
+        legend.position = c(0.15,0.86),
+        legend.background = element_rect(colour="black", size=0.8),
+        legend.key.size = unit(10, "mm"),
+        legend.margin = margin(t=-2,b=4,l=5,r=8)) 
+
+
+
+
+
+
+
+
+
+
+
+
 
 lm_eqnGS <- function(hair.data){
   m <- lm(d13C[Region=="GS"] ~ loc_frst_cap[Region=="GS"], hair.data);
