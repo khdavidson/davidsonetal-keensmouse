014203ba1fcd2ba838b047c05d40f50144ec1d19
kdavidson <katie.davidson92@gmail.com>
2020-09-27 08:30:37 -0700

    added C normalization

--- distshore_focalfood_iso.R
+++ distshore_focalfood_iso.R
@@ -13,6 +13,11 @@ iso.dat <- read.csv("invert_veg_iso.csv")
 
 #                                                             DATA CLEANING 
 
+# cleaning main database 
+iso.dat <- iso.dat %>% 
+  rename(CN_ratio = C.N) %>%
+  print
+
 # Select focal food items:
 
 # Salal
@@ -29,6 +34,83 @@ iso.weevil.dat <- iso.dat %>%
 iso.gbeetle.dat <- iso.dat %>% 
   filter(ID == "ground beetle") %>%
   print()
+
+########################################################################################################################################################
+
+#                                                           LIPID CORRECTION 
+
+##################
+# C:N over space #
+##################
+
+# Salal
+ggplot(iso.salal.dat, aes(x=dist_group, y=CN_ratio)) +
+  geom_point()
+
+min(iso.salal.dat$CN_ratio) # 24.5
+max(iso.salal.dat$CN_ratio) # 82.4
+
+
+# Weevils
+ggplot(iso.weevil.dat, aes(x=dist_group, y=CN_ratio)) +
+  geom_point()
+
+min(iso.weevil.dat$CN_ratio) # 5.5
+max(iso.weevil.dat$CN_ratio) # 6.7
+
+
+# Ground beetles
+ggplot(iso.gbeetle.dat, aes(x=dist_group, y=CN_ratio)) +
+  geom_point()
+
+min(iso.gbeetle.dat$CN_ratio) # 4.1
+max(iso.gbeetle.dat$CN_ratio) # 8.4
+
+# All of these are C:N > 4 and show some level of variability over space, therefore they should be lipid-normalized as per Post et al. 2007
+# However although salal has really  high C:N, Post et al 2007 had no relationship between C:N and Dd13C/% lipid, so we can't use the C:N to correct. 
+# There is a slight relationship though between C:N and Dd13C/%lipid so we can correct for the invert lipid variation. 
+
+
+#######################
+# LIPID NORMALIZATION #
+#######################
+
+# FOR AQUATIC ORGANISMS:
+# d13Cnorm = d13Cuntreated + Dd13C    therefore
+# d13Cnorm = d13Cuntreated - 3.32 + 0.99 * C:N
+
+# FOR TERRESTRIAL ANIMALS then would be: 
+# d13cnorm = d13Cuntreated + Dd13C    therefore
+# d13Cnorm = d13Cuntreated - 3.44 + 1.00 * C:N  (Equation 6, Post et al 2007. Note: only explained 25% of variance)
+
+
+# Salal - making this column anyway just for cbind column matching later on 
+iso.salal.dat <- iso.salal.dat %>% 
+  mutate(d13C_norm = d13C) %>% 
+  print()
+
+ggplot(iso.weevil.dat, aes(x=dist_group, y=d13C_norm)) +
+  geom_point()
+
+
+# Weevils
+  iso.weevil.dat <- iso.weevil.dat %>% 
+  mutate(d13C_norm = d13C-3.44+1*CN_ratio) %>% 
+  print()
+
+ggplot(iso.weevil.dat, aes(x=dist_group, y=d13C_norm)) +
+  geom_point()
+
+
+# Ground beetles
+iso.gbeetle.dat <- iso.gbeetle.dat %>% 
+  mutate(d13C_norm = d13C-3.44+1*CN_ratio) %>% 
+  print()
+
+ggplot(iso.gbeetle.dat, aes(x=dist_group, y=d13C_norm)) +
+  geom_point()
+
+
 
 
 ########################################################################################################################################################
@@ -54,7 +136,6 @@ iso.gbeetle.dat %>%
 
 
 ########################################################################################################################################################
-
 
 #                                                           STATISTICAL COMPARISONS
 
@@ -106,14 +187,14 @@ dunnTest(iso.salal.dat$d13C ~ iso.salal.dat$dist_group)
 #####
 # will continue as above with non-parametric test for equivalent/consistent results
 # Dunn test not needed because only 2 groups
-kruskal.test(iso.weevil.dat$d13C ~ iso.weevil.dat$dist_group)
+kruskal.test(iso.weevil.dat$d13C_norm ~ iso.weevil.dat$dist_group)
 
 #####
 # Ground beetle
 #####
 # will continue as above with non-parametric test for equivalent/consistent results
-kruskal.test(iso.gbeetle.dat$d13C ~ iso.gbeetle.dat$dist_group)
-dunnTest(iso.gbeetle.dat$d13C ~ iso.gbeetle.dat$dist_group)
+kruskal.test(iso.gbeetle.dat$d13C_norm ~ iso.gbeetle.dat$dist_group)
+dunnTest(iso.gbeetle.dat$d13C_norm ~ iso.gbeetle.dat$dist_group)
 
 
 
@@ -128,7 +209,7 @@ combo.df <- rbind(iso.salal.dat, iso.weevil.dat, iso.gbeetle.dat)
 # Summarize mean +/- SE 
 combo.summary <- combo.df %>% 
   group_by(ID, dist_group) %>% 
-  summarize(meanC = mean(d13C), seC = sd(d13C)/sqrt(length(d13C)), meanN = mean(d15N), seN = sd(d15N)/sqrt(length(d15N))) %>% 
+  summarize(meanC = mean(d13C_norm), seC = sd(d13C_norm)/sqrt(length(d13C_norm)), meanN = mean(d15N), seN = sd(d15N)/sqrt(length(d15N))) %>% 
   print()
 
 # manually edit weevil intervals for plotting
@@ -186,9 +267,9 @@ ggplot(data=combo.summary, aes(x=dist_group, y=meanC, group=ID)) +
   geom_errorbar(aes(ymin = meanC-seC, ymax = meanC+seC), colour="black", width=0, size=1.5) + 
   geom_line(size=1.1, colour="gray80") +
   geom_point(aes(shape=ID), fill="gray80", colour="black", size=7, stroke=1.3) +
-  annotate(geom="text", label="a", x=1, y=-28.3, size=10) +
-  annotate(geom="text", label="b", x=2, y=-29.1, size=10) +
-  annotate(geom="text", label="b", x=3, y=-29.7, size=10) +
+  annotate(geom="text", label="a", x=1, y=-28.2, size=10) +
+  annotate(geom="text", label="b", x=2, y=-29.0, size=10) +
+  annotate(geom="text", label="b", x=3, y=-29.6, size=10) +
   #annotate(geom="text", label="a", x=2, y=-26.3, size=10) +
   #annotate(geom="text", label="a", x=4, y=-25.9, size=10) +
   #annotate(geom="text", label="a", x=1, y=-26.1, size=10) +
@@ -198,7 +279,7 @@ ggplot(data=combo.summary, aes(x=dist_group, y=meanC, group=ID)) +
   scale_shape_manual(values=c("Salal"=23, "Weevil"=22, "Ground beetle"=24)) +
   labs(x = "Distance from beach (m)") +
   ylab(expression(paste(delta^{13},'C (\211)'))) +
-  scale_y_continuous(labels=scaleFUN) +
+  scale_y_continuous(labels=scaleFUN, breaks=seq(-30,-22, by=2), limits=c(-30.5,-22)) +
   theme_bw() +
   theme(axis.title = element_text(size=30, face = "bold"),
         axis.title.y = element_text(margin=margin(t=2,l=0,r=4,b=0)),
@@ -210,7 +291,7 @@ ggplot(data=combo.summary, aes(x=dist_group, y=meanC, group=ID)) +
         panel.border = element_rect(size=1.1),
         legend.text = element_text(size=27), 
         legend.title = element_blank(),
-        legend.position = c(0.8,0.5),
+        legend.position = c(0.8,0.4),
         legend.background = element_rect(colour="black", size=0.8),
         legend.key.size = unit(10, "mm"),
         legend.margin = margin(t=-2,b=4,l=5,r=8))
