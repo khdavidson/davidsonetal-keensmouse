164417234f935617109d5faf2426f98edc2a10e6
khdavidson <katie.davidson92@gmail.com>
2021-06-27 14:06:13 -0700

    minor update for MS simplified

--- mouse-ecology/mark_recap.R
+++ mouse-ecology/mark_recap.R
@@ -38,22 +38,26 @@ recap_names <- mr.dat %>%
   group_by(mouse_id) %>%
   summarize(n=n()) %>% 
   filter(n>=3) %>%
-  pull(mouse_id) %>%
+  pull(mouse_id) %>%    
   print()
-
+# Note GF-3R largest distance travelled
 
 # Extract from dataframe 
 recaps <- mr.dat %>% 
   filter(mouse_id%in%recap_names) %>% 
   filter(mouse_id != "GOS-1Br") %>%
-  dplyr::select(mouse_id, x_dd, y_dd) %>%
-  mutate_at(c("x_dd", "y_dd"), as.numeric) %>%
+  #dplyr::select(mouse_id, x_dd, y_dd) %>%
+  #mutate_at(c("x_dd", "y_dd"), as.numeric) %>%
   print()
 # GOS 1-Br removed over note about concerns over duplicated entry 
 
+
+
+
+
 #####################################################################################################################################################
   
-#                                                                        SPATIAL 
+#                                                                        SPATIAL - NOT USED
 
 # prepare coordinates, data, and proj4string
 coords <- recaps[,c("x_dd", "y_dd")]   
